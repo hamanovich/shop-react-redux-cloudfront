935bbf459115d4d0774e219dc7a6a8cc7b735921
Siarhei Hamanovich <siarhei_hamanovich@epam.com>
2021-10-13 20:23:13 +0300

    BFF service

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@ const API_PATHS = {
   product: 'https://j7cuan16ei.execute-api.eu-central-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://q3edi7db78.execute-api.eu-central-1.amazonaws.com/dev/import',
-  bff: 'https://j7cuan16ei.execute-api.eu-central-1.amazonaws.com/dev',
+  bff: 'http://bff-service-dev.eu-central-1.elasticbeanstalk.com',
   cart: 'http://hamanovich-cart-api-dev.eu-central-1.elasticbeanstalk.com/api',
 };
 
